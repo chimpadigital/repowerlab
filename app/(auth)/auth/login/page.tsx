d8b82fb048cf6bbdf44a133c322e546f67f7051c
stefanoracke <stefanoracke@gmail.com>
2024-09-03 17:47:11 -0300

    RL-12

--- app/(auth)/auth/login/page.tsx
+++ app/(auth)/auth/login/page.tsx
@@ -9,9 +9,13 @@ import { Button } from '@nextui-org/button';
 import { login } from '@/services/auth';
 import { deleteTokenCookie, setTokenCookie } from '@/utils/cookieHandler';
 import { useRouter } from "next/navigation";
+import { EyeFilledIcon, EyeSlashFilledIcon } from '@/components/loginIcons';
 
 export default function Login() {
     const [email, setEmail] = useState<string>('admin@example.com');
+    const [isVisible, setIsVisible] = React.useState(false);
+
+    const toggleVisibility = () => setIsVisible(!isVisible);
     const [loading, setLoading] = useState<boolean>(false)
     const [password, setPassword] = useState<string>('password');
     const [error, setError] = useState<string>('');
@@ -27,8 +31,8 @@ export default function Login() {
             if (userData && userData.data && userData.data.access_token) {
                 setTokenCookie(userData.data.access_token);
                 router.push("/");
-            } 
-        }  catch (error) {
+            }
+        } catch (error) {
             setError((error as Error).message);
             setLoading(false)
             console.log(error)
@@ -39,16 +43,31 @@ export default function Login() {
             <Card className="max-w-[400px] w-full flex justify-center py-2 px-6">
                 <div className="w-full flex justify-center items-center gap-4 flex-col pt-8">
                     <AppLogo />
-                    <h1 className={subtitle() + " !w-fit"}>Login</h1>
+                    <h1 className={subtitle() + " !w-fit"}>Login Repowerlab's Admin</h1>
                 </div>
                 <div className="py-8">
                     <form onSubmit={handleSubmit}>
 
                         <Input onChange={(e) => setEmail(e.target.value)} value={email} type="email" variant={'bordered'} label="Email" />
-                        <Input onChange={(e) => setPassword(e.target.value)} value={password} className='pt-4' type="password" variant={'bordered'} label="Password" />
+                        <Input
+                            onChange={(e) => setPassword(e.target.value)}
+                            value={password}
+                            className='pt-4'
+                            endContent={
+                                <button className="focus:outline-none" type="button" onClick={toggleVisibility} aria-label="toggle password visibility">
+                                    {isVisible ? (
+                                        <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                    ) : (
+                                        <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                                    )}
+                                </button>
+                            }
+                            type={isVisible ? "text" : "password"}
+                            variant={'bordered'}
+                            label="Password" />
                         {
                             error != '' &&
-                            <div className="text-danger text-[12px] pt-2">Contraseña o email incorrecto. Intente nuevamente</div>
+                            <div className="text-danger text-[12px] pt-2">Incorrect password or email. Please try again.</div>
                         }
                         <div className="mt-4 flex justify-center w-full">
                             <Button color="primary" type='submit' className='p-4' isLoading={loading} disabled={loading} endContent={<LoginIcon />}>
